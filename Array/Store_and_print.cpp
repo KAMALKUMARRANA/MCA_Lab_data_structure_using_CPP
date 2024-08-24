5deb3297cc25238766e342cdc0aa7b377bcda7b5
kkrana <kamalwindow654@gmail.com>
2024-08-24 20:05:07 +0530

    init

---
+++ Array/Store_and_print.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter size of an array:";
+    cin>>n;
+    cout<<"Value of n: "<<n<<"\n";
+   // int arr[10];
+   // cout<<"Enter element of array:\n";
+    // for(int i=0;i<n;i++){
+    //    cout<<"Enter element of "<<(i+1)<<" :";
+    //    cin<<arr[i]
+    // }
+
+
+
+
+    return 0;
+}
